c3434a5ddd0ec658c192d606d86b6c133080f611
lucas.fritzsche <lucas.fritzsche@accenture.com>
2019-01-28 11:43:25 +0100

    Version 4

---
+++ angular-app.js
@@ -0,0 +1,19 @@
+var myAngularApp = angular.module('myAngularApp', [])
+myAngularApp.controller('mainController', function(){
+
+  $scope.yourName = "Tom";
+
+  $scope.users = [
+    'Tom', 'Tim', 'Tam', 'Robert'
+  ]
+
+  $scope.logIt = function( ){
+    console.log(scope.yourName);
+  }
+
+
+  $scope.validate = function(){
+
+    return $scope.yourName.length > 3;
+  }
+}
