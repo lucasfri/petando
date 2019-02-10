0943264bca5e835cb3c193aedabda9ff78eca656
lucasfri <lucaslebt@gmail.com>
2019-02-10 22:05:16 +0100

    Mit Login Page

---
+++ src/com/servlet/Login.java
@@ -0,0 +1,35 @@
+package com.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Login
+ */
+@WebServlet("/Login")
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public Login() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		if(request.getParameter("userid").equalsIgnoreCase("Max") && request.getParameter("password").equalsIgnoreCase("123")) {
+			response.sendRedirect("index.html");
+		}else response.sendRedirect("http://google.de");
+	}
+
+
+
+}
