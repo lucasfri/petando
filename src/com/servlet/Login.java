f68fb50f2b2988bc816cc1dbe5e4965b824edcca
lucasfri <lucaslebt@gmail.com>
2019-02-11 10:42:56 +0100

    Jetzt mit DB Anbindung

--- src/com/servlet/Login.java
+++ src/com/servlet/Login.java
@@ -1,11 +1,17 @@
 package com.servlet;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 
 /**
  * Servlet implementation class Login
@@ -24,8 +30,38 @@ public class Login extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		if(request.getParameter("userid").equalsIgnoreCase("Max") && request.getParameter("password").equalsIgnoreCase("123")) {
+	
+    private boolean validCredentials(String username, String password) {
+    	
+    	
+    	Connection con;
+    	Statement stmt;
+    	
+    	try {
+    		Class.forName("com.mysql.jdbc.Driver");
+    		
+    		//Step 1: create a connection
+    		con = DriverManager.getConnection("jdbc:mysql://localhost:3306/myDb?useSSL=false", "root", "Hallolucas112");
+    		//Step 2: create a statement
+    		stmt = con.createStatement();
+    		
+    		String sql = "Select * from users where username '" + username;
+    		ResultSet result = stmt.executeQuery(sql);
+    		
+    		while (result.next()){
+    			System.out.println();
+    			return true;
+    		}
+    	} catch (Exception e) {
+    		e.printStackTrace();
+    	}
+    	return false;
+    }
+    
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		
+		if(validCredentials(request.getParameter("userid"), request.getParameter("password"))) {
 			response.sendRedirect("index.html");
 		}else response.sendRedirect("http://google.de");
 	}
